4ff952fdd506c006366eb0dd1b7ec6704c233e14
lee <edy5016@navet.com>
2018-03-10 15:43:51 +0900

    tutorial 5
    디테일뷰를 컴포넌트화
    부모 변수를 자식 컴포넌트에 넘길때 파라미터 바인딩.
    자식이 부모 변수를 받을떄 input 데코레이터 사용.

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {selector} from 'rxjs/operator/publish';
+import {Hero} from '../hero';
 
 @Component({
   selector: 'app-hero-detail',
@@ -6,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./hero-detail.component.css']
 })
 export class HeroDetailComponent implements OnInit {
-
+// 부모에서 넘겨준것을 받음
+  @Input()
+  hero:Hero;
   constructor() { }
 
   ngOnInit() {
