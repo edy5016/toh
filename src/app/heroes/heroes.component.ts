375b75aaeda777d5eb5f13b92bfcc8d699c67122
lee <edy5016@navet.com>
2018-03-10 15:35:50 +0900

    commit

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {Hero} from '../hero';
+import {HEROES} from '../mock-hero';
+
 
 @Component({
   selector: 'app-heroes',
@@ -7,10 +9,8 @@ import {Hero} from '../hero';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  hero:Hero = {
-    id: 1,
-    name : 'Wimdstorm'
-  };
+  heroes = HEROES; // 배열 타입
+  selectedHero : Hero;
 
   constructor() { }
 
